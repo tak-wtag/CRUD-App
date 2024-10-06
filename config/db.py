19d7aeae008fed71e5f479f6b47caec1c669caf3
welldev <welldev@welldevs-Mac-mini.local>
2024-10-06 18:04:28 +0600

    Docker-compose

--- config/db.py
+++ config/db.py
@@ -3,6 +3,6 @@ import psycopg2
 
 #engine = create_engine("postgresql+psycopg2://root:1234@localhost:5433/crud_db")
 meta = MetaData()
-conn = psycopg2.connect(host = "localhost", database = "crud_db" , user = "root", password = "1234" , port = "5433")
+conn = psycopg2.connect(host = "postgres", database = "crud_db" , user = "root", password = "1234" , port = "5432")
 
 
