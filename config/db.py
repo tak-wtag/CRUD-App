e95b696427ffca62a2aa93af14fd5e5ea3f289a0
welldev <welldev@welldevs-Mac-mini.local>
2024-10-23 14:08:50 +0600

    K8s added

--- config/db.py
+++ config/db.py
@@ -1,7 +1,6 @@
-from sqlalchemy import MetaData
+import os
 import psycopg2
+from sqlalchemy import MetaData
 
-#engine = create_engine("postgresql+psycopg2://root:1234@localhost:5433/crud_db")
+conn = psycopg2.connect(host=os.environ.get('DB_HOST'), database=os.environ.get('DB_NAME'), user=os.environ.get('DB_USER'), password=os.environ.get('DB_PASSWORD'), port=os.environ.get('DB_PORT'))
 meta = MetaData()
-conn = psycopg2.connect(host = "postgres", database = "crud_db" , user = "root", password = "1234" , port = "5432")
-
